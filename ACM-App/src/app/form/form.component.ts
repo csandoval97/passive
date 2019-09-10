e84be9a2dfb206cc3ec5c4a62d3ef915de74b403
csando6 <csando6@uic.edu>
2019-09-09 23:10:34 -0500

    finished with some css styling

--- ACM-App/src/app/form/form.component.ts
+++ ACM-App/src/app/form/form.component.ts
@@ -8,8 +8,10 @@ import { User } from '../user';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  
   counter = 0;
   newUser = new User(1,'a','b','c','d','e','f','g','h','i','j','k','l',false,2,3,'m','n','o','p','q',false,4,'r',5);
+
   year:string[] = ["Freshman","Sophomore","Junior","Senior","Grad Masters","Grad PHD","Professor","Alumni","Other"];
   commute:string[]=["Live on Campus", "Walking Distance", ">30 min", ">1 hour","Other"];
   interest:string[]=["Security","Visualization","Computer Human Interaction","Machine Learning","Education","Mathematics","Scalable Computing"];
@@ -32,6 +34,9 @@ export class FormComponent implements OnInit {
   }
   backCounter(){
     this.counter--;
+  }
+  onSubmit(){
+    console.dir(this.newUser);
   }
   constructor() { 
   }
