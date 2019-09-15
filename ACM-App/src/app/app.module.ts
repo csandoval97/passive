d259e2aaceafcff08b3fc880139aec2d424f652e
csando6 <csando6@uic.edu>
2019-09-15 18:38:33 -0500

    visual add

--- ACM-App/src/app/app.module.ts
+++ ACM-App/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
+import { ResetComponent } from './reset/reset.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FormComponent
+    FormComponent,
+    ResetComponent
   ],
   imports: [
     BrowserModule,
