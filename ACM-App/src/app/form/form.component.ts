517f6aede248c5cbab1f76ab26d87ce932603412
csando6 <csando6@uic.edu>
2019-09-15 16:35:57 -0500

    added multivalue array

--- ACM-App/src/app/form/form.component.ts
+++ ACM-App/src/app/form/form.component.ts
@@ -10,18 +10,18 @@ import { User } from '../user';
 export class FormComponent implements OnInit {
   
   counter = 0;
-  newUser = new User(1,'a','b','c','d','e','f','g','h','i','j','k','l',false,2,3,'m','n','o','p','q',false,4,'r',5);
+  newUser = new User(1,'a','b','c','d','e','f','g','h','i','j','k','l',false,2,3,'m',[],[],[],[],false,4,'r',5);
 
   year:string[] = ["Freshman","Sophomore","Junior","Senior","Grad Masters","Grad PHD","Professor","Alumni","Other"];
   commute:string[]=["Live on Campus", "Walking Distance", ">30 min", ">1 hour","Other"];
+  
   interest:string[]=["Security","Visualization","Computer Human Interaction","Machine Learning","Education","Mathematics","Scalable Computing"];
-  sigs:string[]=["SysAdmin","BlockChain","Algorithmic Trading","Math","Windows","None of the above"];
+  sigs:string[]=["SysAdmin","BlockChain","Algorithmic Trading","Math","Windows"];
   majorEvents:string[]=["Flourish!(An open source conference run by LUG and ACM student Orgs)",
                        "Hackathon",
                        "IT D3bug Challenges",
                        "Programming Challenges",
-                       "Reflection Projection",
-                       "Not interested"
+                       "Reflection Projection"
                       ];
   competition:string[]=["Hack Illinois",
                       "ICPC programming competition",
@@ -29,13 +29,56 @@ export class FormComponent implements OnInit {
                       "Loyola Datafest",
                       "DOE Cyberforce Competition"
                       ];
-  nextCounter(){
+  intere = new Array(this.interest.length+1);
+  sig = new Array(this.sigs.length);
+  majorEven = new Array(this.majorEvents.length);
+  competit = new Array(this.competition.length);
+  interestoption = false;
+  interestValue="";
+  
+  makeInterest(){
+    for(let i=0;i<this.interest.length;i++){
+      if(this.intere[i]){
+        this.newUser.interestInCS.push(this.interest[i] );
+      }
+    }
+    if(this.intere[this.intere.length-1]){
+      this.newUser.interestInCS.push(this.interestValue);
+    }
+  }
+  makeSigs(){
+    for(let i=0;i<this.sigs.length;i++){
+      if(this.sig[i]){
+        this.newUser.sigs.push(this.sigs[i] );
+      }
+    }
+  }
+  makeEvents(){
+    for(let i=0;i<this.majorEvents.length;i++){
+      if(this.majorEven[i]){
+        this.newUser.majorEvent.push(this.majorEvents[i] ); 
+      }
+    }
+  }
+  makeCompetition(){
+    for(let i=0;i<this.competition.length;i++){
+      if(this.competit[i]){
+        this.newUser.competing.push(this.competition[i])
+      }
+    }
+  }
+  nextCounter(num){
+    console.dir(num);
     this.counter++;
   }
   backCounter(){
     this.counter--;
   }
   onSubmit(){
+    this.makeInterest();
+    this.makeSigs();
+    this.makeEvents();
+    this.makeCompetition();
     console.dir(this.newUser);
   }
   constructor() { 
