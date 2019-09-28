a4401a57a508cc6fc8006ff0ab1c6c8bad9da138
Bharat Middha <bmiddha99@gmail.com>
2019-09-28 15:44:56 -0500

    added methods to create user, group operations

---
+++ Controllers/ACMADController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using passive.ACMAD;
+
+namespace passive.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ACMADController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ACMADController> _logger;
+
+        public ACMADController(ILogger<ACMADController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+// "userName": "passivetest2",
+// "userPassword": "IceIceBaby123",
+// "UIN": "",
+// "major": "",
+// "college": "",
+// "email": "",
+// "title": "",
+// "phoneNumber": "",
+// "firstName": "",
+// "lastName": "",
+// "membershipNumber": "",
+// "memberType": "",
+// "otherData": ""
+        [HttpPost]
+        public string Post([FromBody]ACMAD.User user) {
+            new ACMAD.AD();
+
+            return ACMAD.User.NewMember(user);
+        }
+    }
+}
